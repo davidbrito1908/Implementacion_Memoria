a735b2ddf2c353b7a7d8e1e1799c431b6d44d521
david <david.brito.pacheco@gmail.com>
2024-03-29 17:47:21 -0400

    primer commit - correspondencia directa

---
+++ directa.cpp
@@ -0,0 +1,62 @@
+/* DAVID BRITO 30521360   */
+
+
+#include <iostream>
+using namespace std;
+
+
+const int bloques = 4;
+const int palabras = 1;
+
+int memoriaDirecta[palabras][bloques];
+
+void inicializarMemoria (){
+    int i;
+    for (i=0;i<bloques;i++){
+        memoriaDirecta[0][i]= -1;
+    }    
+}
+
+void escribirEnBloque(int bloque, int palabra, int direccion){
+    memoriaDirecta[palabra][bloque] = direccion;
+}
+int determinarBloque (int direccion){
+    return direccion % bloques;
+}
+
+int verificarBloque (int bloque, int direccion){
+    int acierto;
+
+    if (memoriaDirecta[0][bloque] == direccion){
+        acierto=1;
+    }else{
+        acierto=0;
+    }
+
+    escribirEnBloque(bloque,0,direccion);
+
+    return acierto;
+}
+
+
+
+int main(){
+    int fallos=0, i, direccion,bloque, estado;
+    string msg;
+    inicializarMemoria();
+
+    while(cin >> direccion){
+        bloque = determinarBloque(direccion);
+        estado = verificarBloque(bloque,direccion);
+
+        if (!estado) {
+            msg = "Fallo";
+            fallos++;
+        }else{
+            msg = "Acierto";
+        }
+        cout << direccion << "   " << msg << "   " << memoriaDirecta[0][0] << "   " << memoriaDirecta[0][1] << "   " << memoriaDirecta[0][2] << "   " << memoriaDirecta[0][3] << "\n";
+    }
+
+    return 0;
+}
