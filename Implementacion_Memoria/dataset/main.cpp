320df830bfeaab8e711455636ddfc7f8edff609c
david <david.brito.pacheco@gmail.com>
2024-06-17 22:30:18 -0400

    Lectura de dataset con comentarios

--- Implementacion_Memoria/dataset/main.cpp
+++ Implementacion_Memoria/dataset/main.cpp
@@ -9,70 +9,59 @@ using namespace std;
 
 int main(){
     int secuencia = 1;
-    int dimension=0,estado;
+    int estado;
 
-
+    //ESCOGER TIPO DE MEMORIA A UTTILIZAR
     MemoriaCompletamenteAsociativa memoria;
+    //MemoriaAsociativaPorConjuntos memoria;
+    //MemoriaCorrespondenciaDirecta memoria;
     string datos[memoria.getPalabras()][memoria.getBloques()];
 
     memoria.inicializarMemoria();
-
-    //cout << setw(10) << "Direccion" << setw(18) << "Acierto/Fallo" << setw(15) << "Bloque 0" << setw(15) << "Bloque 1" << setw(15) << "Bloque 2" << setw(15) << "Bloque 3" << "\n";
-    //cout << setfill('-') << setw(90) << "\n";
-    
     
     long n=0;
-    string msg;
+    //string msg;
     long fallos=0,i;
     string linea;
-    //while(n < dimension){
-    
 
     unsigned t0,  t1;
-    
     t0=clock();
-    cin>>linea; //LEER PRIMERA LINEA
+    //cin>>linea; //LEER PRIMERA LINEA
+    //LEER EL DATASET LINEA A LINEA
     while(cin>>linea){
-        //estado = memoria.procesar(imagenes[n].getEmocion(), &secuencia);
         int tam = linea.size();
+        //RECORRER LA LINEA CARACTER A CARACTER
         for(i=0;i<tam;i++){
             if ((linea[i] != ',') && (linea[i]!= '\n')){
-                estado = memoria.procesar(linea[i], &secuencia);
-                //estado = memoria.procesar(linea[i]);
-                //MENSAJE FALLO O ACIERTO
+
+                estado = memoria.procesar(linea[i], &secuencia); //ASOCIATIVA O POR CONJUNTOS
+                //estado = memoria.procesar(linea[i]); //CORRESPONDENCIA DIRECTA
+                //SUMAR FALLO
                 if (!estado) {
-                    msg = "Fallo";
+                    //msg = "Fallo";
                     fallos++; //CONTAR FALLOS
                 }else{
-                    msg = "Acierto";
+                    //msg = "Acierto";
                 }
 
-                //PREBUSQUEDA DEL SIGUIENTE ELEMENTO
+                //PREBUSQUEDA DEL SIGUIENTE ELEMENTO (SI EL ACTUAL ES UN FALLO)
                 if(i<tam-1 &!estado){
                     estado = memoria.procesar(linea[i+1], &secuencia);
                 } 
-                n++;
+                n++; //CONTAR ELEMENTOS PROCESADOS
             }
         }
-        //cout << "fin linea";
-       
     }
 
-    double porcentaje =100 -((fallos *100) / n);
-    cout << "Fallos:" << fallos << endl;
-    cout << "Cantidad de elementos:" << n << endl;
+    double porcentaje =100 -((fallos *100) / n); //PORCENTAHE DE ACIERTOS
+    cout << "Fallos:" << fallos << endl; //NUMERO DE FALLOS
+    cout << "Cantidad de elementos:" << n << endl; //NUMERO DE ELEMENTOS PROCESADOS
     cout << "Porcentaje de aciertos es de:" << porcentaje << "%"<<endl;
 
     t1 = clock();
     
     double time = (double(t1-t0)/CLOCKS_PER_SEC);
-    cout << "Execution Time: " << time << endl;
-
-
-
-
-
-
+    cout << endl << "Tiempo de ejecucion: " << time << endl; //TIEMPO DE EJECUCION
 
     return 0;
 }
--- Implementacion_Memoria/dataset/mmap.cpp
+++ Implementacion_Memoria/dataset/mmap.cpp
@@ -3,7 +3,7 @@
 #include <sstream>
 #include <iomanip>
 #include "memorias.cpp"
-
+//LIBRERIAS PARA MMAP
 #include <sys/mman.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -12,50 +12,18 @@
 #include <fcntl.h>
 using namespace std;
 
-
-const int MAX_ROWS = 36000;
-const int MAX_COLS = 3;
-
-class imagen{
-    private:
-        int emocion;
-        string data,uso;
-
-    public:
-        void construir(int e, string data, string uso){
-            this->emocion = e;
-            this->data=data;
-            this->uso=uso;
-        }
-
-        int getEmocion(){
-            return this-> emocion;
-        }
-        string getUso(){
-            return this-> uso;
-        }
-};
-
-imagen imagenes[MAX_ROWS];
-string datas[MAX_ROWS][MAX_COLS];
-
 int main(){
     int secuencia = 1;
-    int dimension=0,estado;
+    int estado;
 
-
-    // ---------NUEVO------------
     char *addr, *file_name; 
     int fd;
     struct stat file_st;
     char seekchar, newchar;
-    //const char archivo = "fer2013.csv";
 
-    //file_name = &archivo;
     unsigned t0,  t1;
-    t0=clock();
+    t0=clock(); //INICIAR A MEDIR EL TIEMPO DE EJECUCION
 
-//if( -1 == (fd = open(file_name, O_RDWR))) /* open file in read/write mode*/
     if( -1 == (fd = open("fer2013.csv", O_RDWR))) /* open file in read/write mode*/
     {
         perror("Error opened file \n");
@@ -63,75 +31,65 @@ int main(){
     }
   
     fstat(fd, &file_st); /* Load file status */
-  
-    addr = (char *)mmap(NULL,file_st.st_size, PROT_WRITE, MAP_SHARED, fd, 0); /* map file  */
+    
+    addr = (char *)mmap(NULL,file_st.st_size, PROT_WRITE, MAP_SHARED, fd, 0); /* map file  */ //LECTURA CON MMAP
     if(addr == MAP_FAILED) /* check mapping successful */
     {
         perror("Error  mapping \n");
         exit(1);
     }
 
-    //printf("\nfile contents before:\n%s \n", addr); /* write current file contents */
-  
-    //for(size_t i = 0; i < file_st.st_size; i++) /* replace characters  */
-    /*{
-        if (addr[i] == seekchar) 
-       (    addr[i] = newchar) ;
-    }
-    */
-     
- 
-    int row=0;
-
+    //ESCOGER TIPO DE MEMORIA A UTILIZAR
     MemoriaCompletamenteAsociativa memoria;
+    //MemoriaCorrespondenciaDirecta memoria;
+    //MemoriaAsociativaPorConjuntos memoria;
     string datos[memoria.getPalabras()][memoria.getBloques()];
 
     memoria.inicializarMemoria();
 
-    //cout << setw(10) << "Direccion" << setw(18) << "Acierto/Fallo" << setw(15) << "Bloque 0" << setw(15) << "Bloque 1" << setw(15) << "Bloque 2" << setw(15) << "Bloque 3" << "\n";
-    //cout << setfill('-') << setw(90) << "\n";
-    
-
-
     string msg;
     long fallos=0,elementos=0,n=0;
-    int band=0;
-    while(!band && n<file_st.st_size){
-        band= addr[n] == '\n';
-        n++;
-    }
+    int coma=0;
+    //MIENTRAS NO SE LLEGUE AL TAMAÑO DEL ARCHIVO SE LEE EL CARACTER
     while(n < file_st.st_size){
+        if (addr[n] == ','){
+            //cout<<"HOla";
+            coma++;
+        }
+
         if ((addr[n] != ',') && (addr[n] != '\n')){
-            estado = memoria.procesar(addr[n], &secuencia);
-            //MENSAJE FALLO O ACIERTO
+            estado = memoria.procesar(addr[n], &secuencia); //PARA MEMORIA ASOCIATIVA Y POR CONJUNTOS
+            //estado = memoria.procesar(addr[n]);           //PARA CORRESPONDENCIA DIRECTA
+            
+            //SUMAR FALLO O ACIERTO
             if (!estado) {
-                msg = "Fallo";
+                //msg = "Fallo";
                 fallos++; //CONTAR FALLOS
             }else{
-                msg = "Acierto";
+                //msg = "Acierto";
             }
 
-            //PREBUSQUEDA DEL SIGUIENTE ELEMENTO
+            //PREBUSQUEDA DEL SIGUIENTE ELEMENTO (SI EL ELEMENTO ACTUAL PROVOCÓ UN FALLO)
             if(n<file_st.st_size-1 && !estado){
-                estado = memoria.procesar(addr[n+1], &secuencia);
+                estado = memoria.procesar(addr[n+1], &secuencia); //ASOCIATIVA Y POR CONJUNTOS
+                //estado = memoria.procesar(addr[n+1]); //CORRESPONDENCIA DIRECTA
             }
-            elementos++;
+            elementos++; //NUMERO DE ELEMENTOS PROCESADOS
         }
-        n++;
+        n++; //NUMERO TOTAL DE CARACTERES
     }
-    cout << "n =====" << n << endl;
-    double porcentaje = 100 -((fallos *100) / elementos);
-    cout << "Fallos:" << fallos << endl;
-    cout << "Cantidad de elementos:" << elementos << endl;
+    //cout << "n =" << n << endl;
+    cout << "comas"<<coma<<endl;
+    double porcentaje = 100 -((fallos *100) / elementos); //PORCENTAJE DE ACIERTOS
+    cout << "Fallos:" << fallos << endl; //CANTIDAD DE FALLOS
+    cout << "Cantidad de elementos:" << elementos << endl; //CANTIDAD DE ELEMENTOS PROCESADOS
     cout << "Porcentaje de aciertos es de:" << porcentaje << "%";
 
 
     t1 = clock();
     
     double time = (double(t1-t0)/CLOCKS_PER_SEC);
-    cout << "Execution Time: " << time << endl;
-
-
+    cout << endl << "Tiempo de ejecucion: " << time << endl; // TIEMPO DE EJECUCION
 
 
     return 0;
